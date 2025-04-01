47e7840073bcd05ee5210ea26e2692fcea6e911c
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2025-04-01 16:55:55 -0500

    attempting to set up app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import './css/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="page">
       <h1>Hello World!</h1>
     </div>
   )
+  
 }
 
 export default App
