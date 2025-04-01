7e4f375954655b5d9fad021486b61d3851b3d990
KVanHeise <130683376+KVanHeise@users.noreply.github.com>
2025-04-01 17:02:38 -0500

    app adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import './css/App.css'
 function App() {
 
   return (
+    <>
+
     <div className="page">
+
       <h1>Hello World!</h1>
+      
     </div>
+
+    </>
   )
-  
+
 }
 
 export default App
